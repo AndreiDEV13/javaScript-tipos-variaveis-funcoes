022763fc0ced0792e615da91f274a84200d9ebdb
anDrei <dreeielias@gmail.com>
2023-05-25 19:45:42 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura1234!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Andrei"; 
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
